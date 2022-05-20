c65218fafed202ba460dd8bfb7ba385f8cdd703e
Eslashier <anfelov@gmail.com>
2022-05-20 00:35:36 -0500

    Value objects of traffic created with validations

--- src/main/java/com/SofkaU/DDDChallengue/traffic/values/Quantity.java
+++ src/main/java/com/SofkaU/DDDChallengue/traffic/values/Quantity.java
@@ -1,4 +1,23 @@
 package com.SofkaU.DDDChallengue.traffic.values;
 
-public class Quantity {
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Quantity implements ValueObject<Integer> {
+    private final Integer value;
+
+    public Quantity(Integer value){
+        this.value = Objects.requireNonNull(value);
+        if(this.value<0){
+            throw new IllegalArgumentException("Number of vehicles cannot be negative");
+        }
+        if(this.value>10000){
+            throw new IllegalArgumentException("There is a huge ammount of vehicules please check your quantity");
+        }
+    }
+
+    public Integer value(){
+        return value;
+    }
 }
--- src/main/java/com/SofkaU/DDDChallengue/traffic/values/TimeOfData.java
+++ src/main/java/com/SofkaU/DDDChallengue/traffic/values/TimeOfData.java
@@ -1,4 +1,18 @@
 package com.SofkaU.DDDChallengue.traffic.values;
 
-public class TimeOfData {
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class TimeOfData implements ValueObject<Date> {
+    private final Date value;
+
+    public TimeOfData(Date value){
+        this.value = Objects.requireNonNull(value);
+    }
+
+    public Date value(){
+        return value;
+    }
 }
